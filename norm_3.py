d5a4e52015ef2ee14805a6080883b84c554708a1
malostone <46139250+malostone@users.noreply.github.com>
2019-01-14 02:23:19 +0300

    Update norm_3.py

--- norm_3.py
+++ norm_3.py
@@ -52,3 +52,26 @@ def my_filter(find_x, list):
     return list
 
 print(my_filter(find_x, list))
+
+# Задача-4:
+# Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
+# Определить, будут ли они вершинами параллелограмма.
+
+import math
+
+def side_parl(a: tuple,b: tuple):
+    x1,y1 = a
+    x2,y2 = b
+    side = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    return side
+
+
+def it_parallelogram(point1: tuple, point2: tuple, point3: tuple, point4: tuple):
+    if side_parl(point1,point2) == side_parl(point3,point4) and side_parl(point2,point3) == side_parl(point4,point1):
+        return "Это параллелограмм"
+    else:
+        return "Не параллелограмм"
+    
+
+print(it_parallelogram((1,3), (4,7), (2,8), (-1,4)))
+
